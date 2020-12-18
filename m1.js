c395ee0f1f03e33b0207f4bc0cf5fcfec35a9fe7
agustiansueb <18346074+agustiansueb@users.noreply.github.com>
2020-12-18 08:44:41 +0700

    Update m1.js

--- m1.js
+++ m1.js
@@ -1,4 +1,11 @@
-
-var uri = window.location.toString();if (uri.indexOf(&quot;?m=1&quot;,&quot;?m=1&quot;) &gt; 0) {var clean_uri = uri.substring(0, uri.indexOf(&quot;?m=1&quot;));window.history.replaceState({}, document.title, clean_uri);}
-	
+var uri = window.location.toString();
+if (uri.indexOf("&m=1","&m=1") > 0) {
+  var clean_uri = uri.substring(0, uri.indexOf("&m=1"));
+    window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf("?m=1","?m=1") > 0) {
+  var clean_uri = uri.substring(0, uri.indexOf("?m=1"));
+    window.history.replaceState({}, document.title, clean_uri);
+}
 
